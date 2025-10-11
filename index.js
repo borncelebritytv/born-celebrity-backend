45e45b65d0206bdbd5131410b77de5cce852f9b3
Tameika Watts <borncelebritytv@gmail.com>
2025-10-11 18:07:31 -0500

    Initial backend commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const supabase = require('./supabaseClient');
+
+app.get('/', (req, res) => {
+res.send('Born Celebrity TV backend is live with Express and Supabase');
+});
+
+app.get('/users', async (req, res) => {
+const { data, error } = await supabase
+.from('users')
+.select('*');
+
+if (error) return res.status(500).json({ error: error.message });
+res.json(data);
+});
+
+app.listen(3000, () => {
+console.log('Server running at http://localhost:3000');
+});
