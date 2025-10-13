737dad8d02deecc7ee61243765c6da8269c4a3de
Tameika Watts <borncelebritytv@gmail.com>
2025-10-13 02:55:12 -0500

    Added root route for homepage

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
-const usersRoute = require('./users'); // assumes users.js is in the same folder
+const usersRoute = require('./users');
 
 app.use('/users', usersRoute);
+
+app.get('/', (req, res) => {
+  res.send('Born Celebrity backend is live');
+});
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
